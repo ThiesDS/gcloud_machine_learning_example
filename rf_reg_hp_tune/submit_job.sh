9d8a7a98ad11847d437ebb9a9383290235192b58
Sven Thies <s.thies@traumfewo.me>
2019-07-04 17:59:02 +0200

    Update runtime version

--- rf_reg_hp_tune/submit_job.sh
+++ rf_reg_hp_tune/submit_job.sh
@@ -4,17 +4,17 @@ IFS=$'\n\t'
 
 # Customizable environmental variables
 BUCKET_NAME="zd-ai-platform-performanceattribution-training-bucket"
-JOB_NAME="rf_training$(date +"%Y%m%d_%H%M%S")"
 SCALE_TIER=BASIC
 
 
 
 # Fixed environment variables (DO NOT CHANGE)
+JOB_NAME="rf_training$(date +"%Y%m%d_%H%M%S")"
 JOB_DIR=gs://$BUCKET_NAME/job_dir
 TRAINING_PACKAGE_PATH="./trainer_module/"
 MAIN_TRAINER_MODULE="trainer_module.train"
 REGION="europe-west1"
-RUNTIME_VERSION=1.12
+RUNTIME_VERSION=1.13
 PYTHON_VERSION=3.5
 
 # Send training job to the cloud
