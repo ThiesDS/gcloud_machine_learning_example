59e98e383a0de5d7fed2dd31cdf8364312b5cb85
Sven Thies <s.thies@traumfewo.me>
2019-07-13 15:53:08 +0200

    Align .py script to .ipynb to have the same code

--- singlemachine_rf_reg_hp_tune.py
+++ singlemachine_rf_reg_hp_tune.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import pandas as pd
+import numpy as np
 from google.cloud import storage
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
@@ -11,14 +12,14 @@ from sklearn.externals import joblib
 # Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/sventhies/service_accounts/sven_uses_google_vision_api.json"
 
+## Define necessary variables
+
 # Define bucket name
-bucket_name = 'zd-ai-platform-performanceattribution-training-bucket'
+bucket_id = 'zd-ai-platform-performanceattribution-training-bucket'
 # Build job_dir from bucket name
-job_dir =  bucket_name + str('/job_dir')
-# Get the Bucket Id
-bucket_id = job_dir.split('/')[0]
-# Get the path
-bucket_path = job_dir.lstrip('{}/'.format(bucket_id))
+job_dir =  bucket_name + str('job_dir')
+
+## Read training data from gcs bucket
 
 # Load data from bucket to filesystem
 bucket = storage.Client().bucket(bucket_id)
@@ -35,6 +36,8 @@ blob_labels.download_to_filename('data_train_labels.csv')
 features = pd.read_csv('data_train_features.csv')
 labels = pd.read_csv('data_train_labels.csv')
 
+## Prepare data
+
 # Preprocess data (one-hot-encoding of categorical features)
 features_onehot = pd.get_dummies(features)
 
@@ -45,31 +48,52 @@ labels = labels.values.ravel()
 # Train/Test split for hyperparamter tuning process
 features_onehot_train, features_onehot_test, labels_train, labels_test = train_test_split(features_onehot, labels, test_size=0.20)
 
+## Train ML model
+
 # Define the model
 rf = RandomForestRegressor()
 
+# ===================
 # Define grid to search over
 random_grid = {'n_estimators': [int(x) for x in np.linspace(start = 200, stop = 300, num = 2)],
                'max_depth': [int(x) for x in np.linspace(10, 20, num = 2)]}
 
 # Perform randomized search with cross validation to tune hyperparameters
 rf_randomCV = RandomizedSearchCV(
-	estimator = rf, 
-	param_distributions = random_grid, 
-	scoring = 'neg_mean_squared_error',
-	n_iter = 30, 
-	cv = 3, 
-	random_state=42, 
-	n_jobs = -1)
+    estimator = rf, 
+    param_distributions = random_grid, 
+    scoring = 'neg_mean_squared_error',
+    n_iter = 4, 
+    cv = 3, 
+    random_state=42, 
+    n_jobs = -1)
+
+# Get start time
+t1 = datetime.datetime.now()
 
 # Train the model
 rf_randomCV.fit(features_onehot_train, labels_train)
 
+# Get end time
+t2 = datetime.datetime.now()
+
+# Calculate training time
+train_time_minutes = (t2 - t1).seconds/60
+
 # Predict observations
 labels_pred = rf_randomCV.predict(features_onehot_test)
+# ===================
+
+## Train the model
+#rf.fit(features_onehot_train, labels_train)
+#
+## Predict observations
+#labels_pred = rf.predict(features_onehot_test)
 
 # Calculate the mean accuracy on the given test data and labels.
 mse = mean_squared_error(labels_test, labels_pred)
+
+## Write model to gcs bucket
 
 # Export the model to a file
 model_filename = 'model.joblib'
